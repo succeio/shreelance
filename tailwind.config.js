a51479b3f70e9f55160b5579d4f9775e55413728
HCNX <blackteka@gmail.com>
2026-07-19 16:30:17 +0300

    Уладил разногласия с tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./internal/**/*.go",
+    "./internal/ui/**/*.go"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
